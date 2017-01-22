b9ed4d4b41929396738b2b5934d59f75532e4f98
Ashish Katiyar <katiyar.ashish1989@gmail.com>
2017-01-22 17:08:09 -0500

    Setting Tf weights and biases

--- Assignment/ReadDigit.py
+++ Assignment/ReadDigit.py
@@ -32,3 +32,45 @@ image_width = image_height = np.ceil(np.sqrt(image_size)).astype(np.uint8)
 labels_unroll = train_data[[0]].values.ravel()
 #unique labels, will be used as no. of classes
 unique_labels = np.unique(labels_unroll).shape[0]
+
+#define func to convert dense labels into one_hot_vectors
+def one_hot_convert(dense_labels,num_of_classes):
+	num_of_labels = dense_labels.shape[0]
+	index_offset = np.arange(num_of_labels) * num_of_classes
+	one_hot_lables = np.zeros((num_of_labels, num_of_classes))
+	one_hot_lables.flat[index_offset + dense_labels.ravel()] = 1
+	return one_hot_lables
+	
+#get the full matrix of one_hot_vectors
+labels = one_hot_convert(labels_unroll, unique_labels)
+labels = labels.astype(np.uint8)
+
+#set the validation images and labels
+validation_images = images[:Validation_Size]
+validation_labels = labels[:Validation_Size]
+
+#set the training images seperate from validation images
+train_images = images[Validation_Size:]
+train_labels = labels[Validation_Size:]
+
+#--------------
+#Setting up TensorFlow
+
+#can be experimented with but do not set to 0
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+    
+#can be experimented with but do not set to 0
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+    
+# convolution
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+#can be modified for experimenting with the model
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
